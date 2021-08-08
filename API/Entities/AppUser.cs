c883d82a4a76108b3561db525bd256eebd9a9f5d
Abel Gonzalez <xadein123@gmail.com>
2021-08-07 21:50:13 -0400

    First commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; } // use word Id, Entity framework recognizes 
+                                    //it as our primary key of our database
+        public string UserName { get; set; }
+
+    }
+}
